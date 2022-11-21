051e1a95f95e3e0da5adf5883a70375a3d159d51
dotaveteran <ntkachev03@mail.ru>
2022-11-22 01:36:53 +0500

    Исправление ошибок №2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { createDescription } from "./data.js";
-import { descriptionsCount } from "./util.js"
+import { createDescription } from './data.js';
+import { descriptionsCount } from './util.js';
 
-console.log(Array.from({length: descriptionsCount}).map((value, index) => createDescription(index + 1)));
+Array.from({length: descriptionsCount}).map((value, index) => createDescription(index + 1));
--- js/util.js
+++ js/util.js
@@ -20,8 +20,9 @@ const getRandomPositiveInteger = (a, b) => {
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-}
+};
 
 const checkStringLength = (str, length) => str.length <= length;
+checkStringLength('Пример строки', 35);
 
-export { likesCount, commentsCount, originalityPercentsCount, getRandomPositiveInteger, descriptionsCount }
+export { likesCount, commentsCount, originalityPercentsCount, getRandomPositiveInteger, descriptionsCount };
