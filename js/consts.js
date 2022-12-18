bce32bf04c300b0ffcdbb026a7fe128754a79b0c
dotaveteran <ntkachev03@mail.ru>
2022-12-18 06:16:36 +0500

    помощь друга

--- js/consts.js
+++ js/consts.js
@@ -14,6 +14,33 @@ const MaxHashtag = {
 
 const DEFAULT_COUNT_COMENTS = 5;
 const STEP_COUNT_COMMENTS = 5;
+const SCALE_STEP = 25;
+
+const Effects = {
+  'none': { name: 'none', filter: '', unit: '',
+    options: {range: {min: 0, max: 100}, step: 1, start: 100},
+  },
+  'chrome': {name: 'chrome', filter: 'grayscale', unit: '',
+    options: { range: {min: 0, max: 1}, step: 0.1, start: 1},
+  },
+  'sepia': { name: 'sepia', filter: 'sepia', unit: '',
+    options: { range: {min: 0, max: 1}, step: 0.1, start: 1},
+  },
+  'marvin': { name: 'marvin', filter: 'invert', unit: '%',
+    options: {range: {min: 0, max: 100}, step: 1, start: 100},
+  },
+  'phobos': {name: 'phobos', filter: 'blur', unit: 'px',
+    options: {range: {min: 0, max: 3}, step: 0.1, start: 3},
+  },
+  'heat': {name: 'heat', filter: 'brightness', unit: '',
+    options: {range: {min: 1, max: 3}, step: 0.1, start: 3},
+  }
+};
+
+const ScaleRange = {
+  Min: 25,
+  Max: 100
+};
 
 const ErrorMessage = {
   SPACE_MISSES: 'Хэш-теги должны разделяться пробелами',
@@ -25,4 +52,4 @@ const ErrorMessage = {
   COMMENT_MAX_LENGTH: `Максимальная длина комментария не может превышать ${MAX_COMMENT_LENGTH} символов`
 };
 
-export{MAX_COUNT_PHOTOS, CountLike, MAX_COMMENT_LENGTH, MaxHashtag, ErrorMessage, STEP_COUNT_COMMENTS, DEFAULT_COUNT_COMENTS};
+export{MAX_COUNT_PHOTOS, CountLike, MAX_COMMENT_LENGTH, MaxHashtag, ErrorMessage, STEP_COUNT_COMMENTS, DEFAULT_COUNT_COMENTS, Effects, SCALE_STEP, ScaleRange};
